bbf85658be6e28b834fdda6d501466d9a9b7ed50
pppppowell <aiueonigiri1121@gmail.com>
2019-07-16 16:05:14 +0000

    testuser2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,10 +37,6 @@ class UsersController < ApplicationController
 
   def update
     @user=User.find_by(id: params[:id])
-    if @user.id==5
-      @error_message="テストユーザーの情報は変更できません"
-      render("users/edit")
-    else
       @user.email=params[:email]
       @user.name=params[:name]
 
@@ -56,7 +52,7 @@ class UsersController < ApplicationController
     else
       render("users/edit")
     end
-  end  
+  
   end
 
   def login_form
